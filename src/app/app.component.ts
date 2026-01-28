89d3318a562ec46cf71a078dde70fbaaac37090d
PritishKhobragade <pritishkhobragade1234@gmail.com>
2026-01-28 11:50:24 +0530

    addTaskAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDoList';
+  task ="";
+  taskList:{id:number,task:string}[]=[]
+
+  addTask(){
+    this.taskList.push({id:this.taskList.length+1,task:this.task})
+    this.task=""
+    console.log(this.taskList); 
+  }
+
+
+
 }
